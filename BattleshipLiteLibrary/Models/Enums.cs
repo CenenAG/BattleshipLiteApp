5beb0509c1b907541f871b801cf90fab3a0e7a35
CenenAG <50300777+CenenAG@users.noreply.github.com>
2023-05-23 12:06:51 -0700

    Add project files.

---
+++ BattleshipLiteLibrary/Models/Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace BattleshipLiteLibrary.Models
+{
+    //0=Empty, 1=Ship, 2=Miss, 3=Hit, 4=Sunk
+    public enum GridSpotStatus
+    {
+        Empty,
+        Ship,
+        Miss,
+        Hit,
+        Sunk
+    }
+}
+
